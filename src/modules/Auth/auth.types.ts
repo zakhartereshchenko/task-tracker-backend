61526564ddc3d9d61aaebd68943567e803c4b032
zakhartereshchenko <zahar.tereshencko20012@gmail.com>
2026-04-26 00:17:38 +0200

    .

--- src/modules/Auth/auth.types.ts
+++ src/modules/Auth/auth.types.ts
@@ -1,3 +1,5 @@
-import { User } from "../../generated/prisma/index.js";
+// import { User } from "../../generated/prisma/index.js";
+
+import { User } from "@prisma/client";
 
 export type LoginData = Pick<User, "username" | "password">
